1bea0239f65578f5aa9603e247f33d98a09daa6d
MugoBrian <mikanmuchiri@gmail.com>
2025-09-09 07:54:45 +0300

    Modularization of all exported components.

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import NavigationBar from "./NavigationBar";
+import Footer from "./Footer";
+import GoalCard from "./GoalCard";
+import AddGoalForm from "./AddGoalForm";
+import AddContributionFrom from "./AddContributionForm";
+import FormModal from "./FormModal";
+
+export {
+  NavigationBar,
+  Footer,
+  GoalCard,
+  AddGoalForm,
+  AddContributionFrom,
+  FormModal,
+};
