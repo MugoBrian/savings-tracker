0c1338af0bc7b77a6771a3edaabf32fc76cc231e
MugoBrian <mikanmuchiri@gmail.com>
2025-09-09 07:45:07 +0300

    Removed unnecessary comments.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar } from "react-bootstrap"; // Keep React Bootstrap Navbar import
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/image.png";
 
@@ -7,35 +7,33 @@ function NavigationBar() {
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      {/* <Container> */}
-        <Navbar.Brand as={Link} to="/">
-          <img
-            src={Logo}
-            alt="NissMart Logo"
-            className="d-inline-block align-center"
-            width="50"
-            height="50"
-          />
-          NisSavings
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav " >
-          <Nav className="me-auto justify-content-end">
-            {navItems.map((item, index) => (
-              <Nav.Link
-                as={Link}
-                key={index}
-                to={item.toLowerCase()}
-                className={({ isActive }) =>
-                  `${isActive ? `font-bold` : `font-normal`}`
-                }
-              >
-                {item}
-              </Nav.Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      {/* </Container> */}
+      <Navbar.Brand as={Link} to="/">
+        <img
+          src={Logo}
+          alt="NissMart Logo"
+          className="d-inline-block align-center"
+          width="50"
+          height="50"
+        />
+        NisSavings
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav ">
+        <Nav className="me-auto justify-content-end">
+          {navItems.map((item, index) => (
+            <Nav.Link
+              as={Link}
+              key={index}
+              to={item.toLowerCase()}
+              className={({ isActive }) =>
+                `${isActive ? `font-bold` : `font-normal`}`
+              }
+            >
+              {item}
+            </Nav.Link>
+          ))}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
